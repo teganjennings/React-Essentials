8d75e5becfed842700932749d0e6ff5ae419d19a
Tegan Jennings <teganjennings1@hotmail.com>
2024-01-09 23:29:48 +0000

    Adding Core Concept Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,58 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
+	return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description = reactDescriptions[getRandomInt(2)];
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
+	const description = reactDescriptions[getRandomInt(2)];
+	return (
+		<header>
+			<img src={reactImg} alt="Stylized atom" />
+			<h1>React Essentials</h1>
+			<p>
+				{description} React concepts you will need for almost any app you are
+				going to build!
+			</p>
+		</header>
+	);
+}
+
+function CoreConcept({ image, title, description }) {
+	return (
+		<li>
+			<img src={image} alt={title} />
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</li>
+	);
 }
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main>
+				<section id="core-concepts">
+					<h2>Core Concepts</h2>
+					<ul>
+						<CoreConcept {...CORE_CONCEPTS[0]} />
+						<CoreConcept {...CORE_CONCEPTS[1]} />
+						<CoreConcept {...CORE_CONCEPTS[2]} />
+						<CoreConcept
+							title={CORE_CONCEPTS[3].title}
+							description={CORE_CONCEPTS[3].description}
+							image={CORE_CONCEPTS[3].image}
+						/>
+					</ul>
+				</section>
+				<h2>Time to get started!</h2>
+			</main>
+		</div>
+	);
 }
 
 export default App;
