b780aadb4095804ab78e6a21d2ce6bcb0f7c1134
Tegan Jennings <teganjennings1@hotmail.com>
2024-01-09 23:50:50 +0000

    47: Storing Component Style Files

---
+++ src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,11 @@
+import "./CoreConcepts.css";
+
+export default function CoreConcept({ image, title, description }) {
+	return (
+		<li>
+			<img src={image} alt={title} />
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</li>
+	);
+}
